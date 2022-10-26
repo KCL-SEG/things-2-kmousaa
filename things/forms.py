70cb9de1c984ecc14f376efb41f2ae68fc5e8d3f
Karim Mousa <karimmousa202@gmail.comgit config --global --replace-all user.email karimmousa202@gmail.com>
2022-10-26 11:45:01 +0100

    attempt 1

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
 
-# Create your forms here.
+from django import forms
+from .models import Thing
+
+class SignUpForm(forms.ModelForm):
+    #allows us to relate form to models
+    class Meta:
+        model = Thing;
+        fields = ('name', 'description' , 'quantity')
+        widgets = {'description' :forms.Textarea(),'quantity':forms.NumberInput()}
+
+
+#now if we change validation constraints within user models - form changes as well
